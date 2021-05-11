ef21b07e3e2db0f3612467f2193d85a4626cfea9
DayaToledo <dayanabritotoledo_@hotmail.com>
2021-05-11 16:52:07 -0300

    feat: add DynamoDB and S3 bucket

---
+++ src/libs/dynamodb-lib.js
@@ -0,0 +1,13 @@
+import AWS from "aws-sdk";
+
+AWS.config.update({ region: "sa-east-1" });
+
+const client = new AWS.DynamoDB.DocumentClient();
+
+export default {
+    get: (params) => client.get(params).promise(),
+    put: (params) => client.put(params).promise(),
+    query: (params) => client.query(params).promise(),
+    update: (params) => client.update(params).promise(),
+    delete: (params) => client.delete(params).promise(),
+};
