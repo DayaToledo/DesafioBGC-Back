e5d666a074553f89344dd0571f80540f97a3b64a
DayaToledo <dayanabritotoledo_@hotmail.com>
2021-05-11 16:54:36 -0300

    feat: add handler function

---
+++ src/libs/handler-lib.js
@@ -0,0 +1,25 @@
+export default function handler(lambda) {
+
+    return async function (event, context) {
+        let body, statusCode;
+
+        try {
+            body = await lambda(event, context);
+            statusCode = 200;
+        } catch (e) {
+            console.log(e);
+
+            body = { error: e.message };
+            statusCode = 500;
+        }
+
+        return {
+            statusCode,
+            body: JSON.stringify(body),
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true,
+            },
+        };
+    };
+}
